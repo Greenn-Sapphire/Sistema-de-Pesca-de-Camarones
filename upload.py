8388023960694742455a48c8868a5a47981559cd
Greenn-Sapphire <brayann117@hotmail.es>
2023-05-30 01:47:10 -0500

    Cambios en upload Frame y visual Frame

--- upload.py
+++ upload.py
@@ -1,10 +1,11 @@
 import customtkinter as ctk
-from tkinter import ttk
 import pandas as pd
 from Recursos.style import Estilo
 from tkinter.filedialog import askopenfilename
+from CTkMessagebox import CTkMessagebox
 from CTkTable import *
 from Archivos import *
+import configparser
 
 class upload(ctk.CTkFrame):
 	def __init__(self, master):
@@ -15,49 +16,57 @@ class upload(ctk.CTkFrame):
 		self.grid(sticky= 'nswe')
 
 		self.uploadbutton = ctk.CTkButton(self, text="Cargar archivo", command=self.button_callbck)
-		self.uploadbutton.grid(row = 0, column = 0, sticky= 'w')
+		self.uploadbutton.grid(row = 0, column = 1, sticky= 'w', pady=8)
 		self.connectbutton = ctk.CTkButton(self, text="Conectar a una base de datos", command=self.button_callbck)
-		self.connectbutton.grid(row = 0, column = 1, sticky= 'e')
+		self.connectbutton.grid(row = 0, column = 1, sticky= 'e', pady=8)
 		#self.filterbutton = ctk.CTkButton(self, text="Conectar a una base de datos", command=self.button_callbck)
 		#self.filterbutton.grid(row = 0, column = 1, sticky= 'e')
 
+		self.infoFrame = ctk.CTkFrame(self, corner_radius = 5)
+		self.infoFrame.grid(row = 0, column = 0, rowspan = 2, sticky= 'nwse', padx=8, pady=8)
+		self.infoFrame.grid_columnconfigure(0, weight=1)
+		self.infoFrame.grid_rowconfigure(8, weight=1)
+		self.infoFrameLabel = ctk.CTkLabel(self.infoFrame, text="Información", compound="left", font=ctk.CTkFont(size=15, weight="bold"))
+		self.infoFrameLabel.grid(row=0, column=0, padx=20, pady=20)
 		
-
+		self.frameInfo = ctk.CTkLabel(self.infoFrame, text = 'Número de columnas', corner_radius=5, font=ctk.CTkFont(size=12, weight="bold"))
+		self.frameInfo.grid(row=1, column = 0, sticky='n',  padx=5, pady=2)
+		self.frameInfo = ctk.CTkLabel(self.infoFrame, text = 'Número de registros', corner_radius=5, font=ctk.CTkFont(size=12, weight="bold"))
+		self.frameInfo.grid(row=3, column = 0, sticky='n',  padx=5, pady=2)
+		self.frameInfo = ctk.CTkLabel(self.infoFrame, text = 'Registros repetidos', corner_radius=5, font=ctk.CTkFont(size=12, weight="bold"))
+		self.frameInfo.grid(row=5, column = 0, sticky='n',  padx=5, pady=2)
+		self.frameInfo = ctk.CTkLabel(self.infoFrame, text = 'Registros vacios', corner_radius=5, font=ctk.CTkFont(size=12, weight="bold"))
+		self.frameInfo.grid(row=7, column = 0, sticky='n',  padx=5, pady=2)
+		
 	def button_callbck(self):
-		filename = askopenfilename(initialdir="C://")
-		dataframe = pd.read_csv(filename, sep=',|;', engine="python", on_bad_lines='skip')
+		try:
+			filename = askopenfilename(initialdir="C://")
+			dataframe = pd.read_csv(filename, sep=',|;', engine="python", on_bad_lines='skip')
+			settings = configparser.ConfigParser()
+			settings.read('config.ini')
+			settings['SETTINGS']['dataframe_path'] = filename
+			with open('config.ini', 'w') as configfile:
+				settings.write(configfile)
 
-		if 'table' in locals():
-			self.frame.destroy()
+			if 'table' in locals():
+				self.frame.destroy()
+				self.frameInfo.destroy()
 
-		self.frame = ctk.CTkScrollableFrame(self, orientation='horizontal', fg_color='transparent')
-		self.frame.grid(row = 1, column = 0, sticky = 'nswe', columnspan=2, padx=10, pady=10)
-		table = CTkTable(self.frame, column_names=[dataframe.columns.to_numpy().tolist()] ,values=dataframe.to_numpy().tolist())
-		table.grid(row= dataframe.shape[0], column= dataframe.shape[1])
+			self.frame = ctk.CTkScrollableFrame(self, orientation='horizontal', fg_color='transparent')
+			self.frame.grid(row = 1, column = 1, sticky = 'nswe', padx=10, pady=5)
+			table = CTkTable(self.frame, row = 20, column_names=[dataframe.columns.to_numpy().tolist()] ,values=dataframe.to_numpy().tolist())
+			table.grid(row= dataframe.shape[0], column= dataframe.shape[1])
 
-		"""
-		tableframe = ttk.Treeview(self.frame, columns = dataframe.columns, show = "headings")
-		for column in dataframe.columns:
-			tableframe.heading(column, text=column)
-		tableframe.grid(row=1, column=0, columnspan=2)
+			self.frameInfo = ctk.CTkLabel(self.infoFrame, text = len(dataframe.columns))
+			self.frameInfo.grid(row=2, column = 0, sticky='n')
 
-		for i, (name, score) in enumerate(dataframe, start=1):
-			tableframe.insert("", "end", values=(i, name, score))
-		
-		#Codigo de tabla que funciona
-		self._widgets = []
-		for row in range(dataframe.shape[0]):
-			current_row = []
-			for column in range(dataframe.shape[1]):
-				if row == 0:
-					label = ctk.CTkLabel(self.frame, text=dataframe.columns[column], width=10)
-					label.grid(row=row, column=column, sticky="we", padx=1, pady=1)
+			self.frameInfo = ctk.CTkLabel(self.infoFrame, text = len(dataframe.index))
+			self.frameInfo.grid(row=4, column = 0, sticky='n')
+	
+			self.frameInfo = ctk.CTkLabel(self.infoFrame, text = dataframe.duplicated().sum())
+			self.frameInfo.grid(row=6, column = 0, sticky='n')
 
-				label = ctk.CTkLabel(self.frame, text=dataframe.iloc[row, column], width=10)
-				label.grid(row=row+1, column=column, sticky="we", padx=1, pady=1)
-				current_row.append(label)
-			self._widgets.append(current_row)
-
-		for column in range(dataframe.shape[1]):
-			self.grid_columnconfigure(column, weight=1)
-		"""
+			self.frameInfo = ctk.CTkLabel(self.infoFrame, text = dataframe.isnull().sum())
+			self.frameInfo.grid(row=8, column = 0, sticky='n')
+		except:
+			CTkMessagebox(title="Error", message="El archivo es inválido", icon="cancel")
--- visual.py
+++ visual.py
@@ -1,13 +1,56 @@
 import customtkinter as ctk
 from Recursos.style import Estilo
+import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+from matplotlib.figure import Figure
+import pandas as pd
+import configparser
+import matplotlib.pyplot as plt
 
 class visual(ctk.CTkFrame):
 	def __init__(self, master):
 		super().__init__(master)
 		estilo = Estilo()
+		self.grid_rowconfigure(1, weight=1)
+		self.grid_columnconfigure(1, weight=1)
+		self.grid(sticky= 'nswe')
 
-		self.button = ctk.CTkButton(self, text="Botón", command=self.button_callbck)
-		self.button.grid(row = 2, column = 1,sticky= 'n')
+		self.graphic_menu_label = ctk.CTkLabel(self, text="Tipo de gráfica", compound="left")
+		self.graphic_menu_label.grid(row=0, column=0, padx=20, pady=20, sticky="e")
+		self.graphic_menu = ctk.CTkOptionMenu(self, values=[ "Barras", "Pastel"], command=self.generate_graphic)
+		self.graphic_menu.grid(row=0, column=1, padx=20, pady=20, sticky="e")
 
-	def button_callbck(self):
-		print("Botón ventana 1")
+		self.graphicFrame = ctk.CTkFrame(self, fg_color='white')
+		self.graphicFrame.grid(row = 1, column = 0,sticky= 'nswe', columnspan=2)
+		#generate_graphic(self, 'Barras')
+
+	def generate_graphic(self, graphic_option):
+		#dataframe = pd.read_csv(path=None, sep=',|;', engine="python", on_bad_lines='skip')
+
+		if 'fig' in locals():
+			self.frame.destroy()
+		# the figure that will contain the plot
+		fig = plt.subplots(figsize = (12, 5))
+
+		path = configparser.ConfigParser()
+		path.read('config.ini')
+		dataframe = pd.read_csv(str(path.get('SETTINGS', 'dataframe_path')))
+
+		plot1 = fig.add_subplot(111)
+		plot1.plot(dataframe.plot(x = "NombreServicio", y = "Adultos", kind="bar"))
+
+		# creating the Tkinter canvas
+		# containing the Matplotlib figure
+		canvas = FigureCanvasTkAgg(fig, master = self.graphicFrame)  
+		canvas.draw()
+
+		# placing the canvas on the Tkinter window
+		canvas.get_tk_widget().pack()
+
+		# creating the Matplotlib toolbar
+		toolbar = NavigationToolbar2Tk(canvas, self.graphicFrame)
+		toolbar.update()
+
+		# placing the toolbar on the Tkinter window
+		canvas.get_tk_widget().pack()
